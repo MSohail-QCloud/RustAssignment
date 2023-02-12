097a54ab4950b301fef95986d8ccdf4bd5481f6c
Muhammad Sohail <msohail9x@gmail.com>
2023-02-12 12:08:10 +0500

    c

---
+++ findAverageMarks/src/main.rs
@@ -0,0 +1,32 @@
+use std::io;
+fn main() {
+    println!("Welcome to Percetage Calculator");
+    println!("Please input your Total Marks.");
+
+    let mut totalmarks = String::new();
+
+    io::stdin()
+        .read_line(&mut totalmarks)
+        .expect("Failed to get total marks");
+
+    println!("Your total marks is {totalmarks}");
+    println!("Please input your Number of Subjects.");
+
+    let mut  numbofsubj = String::new();
+
+    io::stdin()
+        .read_line(&mut numbofsubj)
+        .expect("Failed to get Number of Subjects");
+
+    println!("Your subjects are {numbofsubj}");
+
+    let marks_float: f64 = totalmarks.trim().parse().unwrap();
+    let subjects_float: f64 = numbofsubj.trim().parse().unwrap();
+    let average : f64= (marks_float/subjects_float)*100.0;
+
+    println!("Your marks percentage is {average}")
+
+
+
+
+}
